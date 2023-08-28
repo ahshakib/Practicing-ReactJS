72ff6bccc635529833563e339b8b968c1bbd4287
ahshakib <ahshakib75@gmail.com>
2023-08-29 02:39:58 +0600

    Styling counter app

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,30 @@ import "./App.css";
 
 function App() {
   const [count, setCount] = useState(0);
-
+  const styling = {
+    color: "red",
+  };
+  const fontsize = {
+    "font-size": "25px",
+  };
+  const divcolor = {
+    color: "orange",
+    "font-size": "25px",
+    "font-weight": "bold"
+  };
   return (
     <>
-      <h1>Counter App</h1>
-      <button onClick={() => setCount(count + 1)}>+Increment</button>
-      <button onClick={() => setCount(count - 1 >= 0 ? count - 1 : 0)}>-Decrement</button>
-      <div>{count}</div>
+      <h1 style={styling}>Counter App</h1>
+      <button style={fontsize} onClick={() => setCount(count + 1)}>
+        +
+      </button>
+      <button
+        style={fontsize}
+        onClick={() => setCount(count - 1 >= 0 ? count - 1 : 0)}
+      >
+        -
+      </button>
+      <div style={divcolor}>{count}</div>
     </>
   );
 }
