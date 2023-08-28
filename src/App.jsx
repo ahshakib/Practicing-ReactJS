b981c2260e0772e39938bc6c15f3517ac3a39313
ahshakib <ahshakib75@gmail.com>
2023-08-29 02:22:57 +0600

    Improved counter app,dekhlei bujhba!!!!

--- src/App.jsx
+++ src/App.jsx
@@ -3,22 +3,19 @@ import "./App.css";
 
 function App() {
   const [count, setCount] = useState(0);
-  
+
   return (
     <>
       <h1>Counter App</h1>
       <button onClick={() => setCount(count + 1)}>+Increment</button>
-      <button onClick={() => setCount(count - 1)}>-Decrement</button>
+      <button onClick={() => setCount(count - 1 >= 0 ? count - 1 : 0)}>-Decrement</button>
       <div>{count}</div>
-      <Extra/>
     </>
   );
 }
 
-function Extra () {
-  return (
-    <h6>Powered by Shakib</h6>
-  )
+export function Extra() {
+  return <h6>Powered by Shakib</h6>;
 }
 
 export default App;
