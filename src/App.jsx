20154b7ce9cfea001aa87d1b59836109d922ce13
ahshakib <ahshakib75@gmail.com>
2023-08-29 02:51:12 +0600

    Improved styling in counter app

--- src/App.jsx
+++ src/App.jsx
@@ -3,21 +3,24 @@ import "./App.css";
 
 function App() {
   const [count, setCount] = useState(0);
-  const styling = {
-    color: "red",
-  };
+
   const fontsize = {
     "font-size": "25px",
   };
- 
+
   return (
     <>
-      <h1 style={{color:"ActiveBorder"}}>Counter App</h1>
-      <button style={fontsize} className="btn" onClick={() => setCount(count + 1)}>
+      <h1 style={{ color: "ActiveBorder" }}>Counter App</h1>
+      <button
+        style={fontsize}
+        className="btn"
+        onClick={() => setCount(count + 1)}
+      >
         +
       </button>
       <button
-        style={fontsize} className="btn"
+        style={fontsize}
+        className="btn"
         onClick={() => setCount(count - 1 >= 0 ? count - 1 : 0)}
       >
         -
